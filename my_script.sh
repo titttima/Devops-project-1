958d8e8de4a7fc2d106832f76a4bf916e6b27fb6
Fatma <hamzafatma@gmail.com>
2025-07-21 17:49:15 +0200

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -1,3 +1,4 @@
+# Step 1: Clone the repository
 touma@touma-tablet:~$ git clone git@github.com:titttima/Devops-project-1.git
 Cloning into 'Devops-project-1'...
 remote: Enumerating objects: 32, done.
@@ -5,4 +6,11 @@ remote: Counting objects: 100% (32/32), done.
 remote: Compressing objects: 100% (21/21), done.
 remote: Total 32 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
 Receiving objects: 100% (32/32), 11.45 KiB | 902.00 KiB/s, done.
+#!/bin/bash
+
+# Step 2: Navigate into the correct project directory
+cd Devops-project-1/"ABC Technologies"
+
+# Step 3: Build the project using Maven
+mvn clean install
 
