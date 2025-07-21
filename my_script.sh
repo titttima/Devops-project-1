854449f4d28100ea3b080522eded0141df66c06f
tittima <hamzafatma@gmail.com>
2025-07-21 16:46:35 +0200

    Update script with Step 2

--- my_script.sh
+++ my_script.sh
@@ -1,2 +1,8 @@
-nano my_script.sh
+touma@touma-tablet:~$ git clone git@github.com:titttima/Devops-project-1.git
+Cloning into 'Devops-project-1'...
+remote: Enumerating objects: 32, done.
+remote: Counting objects: 100% (32/32), done.
+remote: Compressing objects: 100% (21/21), done.
+remote: Total 32 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+Receiving objects: 100% (32/32), 11.45 KiB | 902.00 KiB/s, done.
 
